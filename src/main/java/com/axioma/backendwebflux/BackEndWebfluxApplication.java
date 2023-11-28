48aaebbad1f34eab495a73fde2b3fd080ed1977b
RafaelTalavera <rtalavera@live.com.ar>
2023-11-28 10:05:28 -0300

    initial commit add dependecy

---
+++ src/main/java/com/axioma/backendwebflux/BackEndWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.axioma.backendwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axioma/backendwebflux/BackEndWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axioma.backendwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
